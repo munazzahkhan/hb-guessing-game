57edee5720e153f37e1bd21c26c7f39da1d22967
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-04-27 20:52:59 +0000

    Looped till the player guessed right number

--- game.py
+++ game.py
@@ -7,6 +7,28 @@ print("Howdy, what's your name?")
 
 # input the name and save it to a variable
 name = input("Type in your name: ")
+print("I am thinking of a number between 1 and 100")
 
 # we need to generate a random number between 1 and 100
 number = random.randint(1,100)
+
+count = 0
+
+# loop till the number is equal to the guess
+while True:
+    #prompt the player to guess the number
+    guess = int(input("Guess the number >  "))
+    if guess < number:
+        print("Your guess is too low, try again.")
+        count += 1
+    elif guess > number:
+        print("Your guess is too high, try again.")
+        count += 1
+    else:
+        print("You are correct.")
+        print(f"You guessed in {count} tries.")
+        break
+    
+
+
+
